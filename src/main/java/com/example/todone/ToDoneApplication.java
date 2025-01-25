f8efcbdcd39798092f34fc000d32909dfdb5e279
matiqof <matiqof@gmail.com>
2025-01-25 12:02:41 +0300

    #1 Инициализация проекта

---
+++ src/main/java/com/example/todone/ToDoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.todone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todone/ToDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
